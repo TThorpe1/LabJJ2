97cf5e4db1ab15765b15629ec811c96451ce893b
terri <thorpetann@gmail.com>
2019-01-14 16:53:25 -0500

    initial commit

---
+++ LabJJ2/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using LabJJ2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabJJ2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public ActionResult Edit(ProductsModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToRoute(new { controller = "Products", action = "Index" });
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ LabJJ2/Models/ProductsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabJJ2.Models
+{
+    public class ProductsModel
+    {
+        [Required(ErrorMessage = "Enter Name")]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Description")]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Enter Price")]
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+                
+        [MaxLength(5)]
+        public string CustomerRating { get; set; }
+    }
+}
